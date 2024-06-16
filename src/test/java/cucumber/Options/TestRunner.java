d161da9c5228f6139194ac98e6b7aa25bcd2a15e
JIH <mhdnurfaizzy@yahoo.com>
2024-06-16 20:33:43 +0700

    create test runner with cucumberOptions

---
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -0,0 +1,14 @@
+package cucumber.Options;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/feature", glue={"stepDefinitions"})
+
+
+public class TestRunner {
+
+
+}
